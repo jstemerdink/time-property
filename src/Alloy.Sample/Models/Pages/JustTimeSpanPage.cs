8d230c2032bd9d648c901770efdc870f9a0e7e76
Bartosz Sekula <Bartosz.Sekula@episerver.com>
2021-12-01 14:37:10 +0100

    Added JustTimeSpanPage.cs

---
+++ src/Alloy.Sample/Models/Pages/JustTimeSpanPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Advanced.CMS.TimeProperty;
+using EPiServer.DataAnnotations;
+
+namespace Alloy.Sample.Models.Pages
+{
+    [SiteContentType(GUID = "123C8A41-5C8C-4BE0-8E73-520FF3DE8267")]
+    [SiteImageUrl(Global.StaticGraphicsFolderPath + "page-type-thumbnail-standard.png")]
+    public class JustTimeSpanPage : SitePageData
+    {
+        [BackingType(typeof(TimeProperty))]
+        [TimePropertySettings(TimePattern = "HH:mm")]
+        public virtual TimeSpan? Time1 { get; set; }
+    }
+}
